6613ea010cc1d261c617e5c042aeb5e87b54c124
Eivind Bjørnli <111429430+h671443@users.noreply.github.com>
2026-03-14 21:58:22 +0100

    Print out header information  (#2)
    * list and unpack recieved packet
    * list header information
    
    ---------
    
    Co-authored-by: h671443 <eivind.bjornli@gmail.com>

--- python/main.py
+++ python/main.py
@@ -1,7 +1,30 @@
 from listen import create_socket
+import struct
 
 sock = create_socket()
 
+
 while True:
-    data, addr = sock.recvfrom(2048)  # F1 packets vary in size
-    print(f"Received {data} bytes from {addr}")
+    data, addr = sock.recvfrom(2048) # F1 packets vary in size
+   # print(list(data))
+    
+    header_values=struct.unpack_from("<HBBBBBQfLLBB", data, 0)
+
+    header = {
+    "packet_format": header_values[0],
+    "game_year": header_values[1],
+    "Version": header_values[2],
+    "minor_version": header_values[3],
+    "packetversion": header_values[4],
+    "packet_ID": header_values[5],
+    "Session_UID": header_values[6],
+    "timestamp": header_values[7],
+    "frameIdentifier": header_values[8],
+    "value2": header_values[9],
+    "playerCarIndex": header_values[10],
+    "secondaryPlayerCarIndex": header_values[11],
+}
+
+    print(list(data))
+    print (header)
+
---
+++ python/socket_reciever.py
@@ -0,0 +1,20 @@
+import socket
+import struct
+UDP_IP = "127.0.0.1"
+UDP_PORT = 5005
+
+sock = socket.socket()
+sock = socket.socket(socket.AF_INET, # Internet
+                      socket.SOCK_DGRAM) # UDP
+
+sock.bind((UDP_IP, UDP_PORT))
+
+while True:
+    data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
+    print ("data printing:")
+    print(list(data))
+    print (data.hex())
+   # print( data % "received message: %s")
+
+
+
