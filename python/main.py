953064eea813267da7a9cede492a9c103bd25615
h671443 <eivind.bjornli@gmail.com>
2026-03-14 23:36:52 +0100

    Add match statement parse different packets. Move header parsing away from main

--- python/main.py
+++ python/main.py
@@ -1,30 +1,45 @@
 from listen import create_socket
+from parser import parse_header
 import struct
 
 sock = create_socket()
 
-
 while True:
     data, addr = sock.recvfrom(2048) # F1 packets vary in size
    # print(list(data))
-    
-    header_values=struct.unpack_from("<HBBBBBQfLLBB", data, 0)
 
-    header = {
-    "packet_format": header_values[0],
-    "game_year": header_values[1],
-    "Version": header_values[2],
-    "minor_version": header_values[3],
-    "packetversion": header_values[4],
-    "packet_ID": header_values[5],
-    "Session_UID": header_values[6],
-    "timestamp": header_values[7],
-    "frameIdentifier": header_values[8],
-    "value2": header_values[9],
-    "playerCarIndex": header_values[10],
-    "secondaryPlayerCarIndex": header_values[11],
-}
 
-    print(list(data))
+    header = parse_header(data)
+
+    # different packet types based of TimeTrail 
+    match header["packet_ID"]:
+      case 0: #Motion packet. Car  movement when player is in control 
+        pass #TODO 
+      case 1: # Session packets 
+        pass #TODO 
+      case 2: # Lap Data packets
+        pass #TODO
+      case 3: #Event packets. specific events in the game like start of session, fastest lap, retirement etc
+        print("hello there. this is an event packet. not much to see here yet")
+      case 11: # Session History packetes. Lap and tyre data for the session.
+        pass #TODO 
+      case 12: #Tyrpe set packet. Tyre data for the session.
+        pass #TODO
+      case 13:  #Motion Ex. 
+        pass #TODO
+      case 14: #TimeTrail packets,
+        pass #TODO
+      
+          
     print (header)
+    print ("packetID:", packetID := header["packet_ID"])
+
+
+
+          
+    
+
+  # print("size:", header_size)
+    #motion packets 
+
 
